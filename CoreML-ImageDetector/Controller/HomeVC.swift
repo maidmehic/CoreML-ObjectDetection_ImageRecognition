d8fd5ba3d9881a2ad57ceb75b06168b702cd0019
Maid Mehic <maid.mehic@compusight.com>
2020-11-21 18:28:43 +0100

    Add HomeVC, MoreVC, mldata model and perform checking

---
+++ CoreML-ImageDetector/Controller/HomeVC.swift
@@ -0,0 +1,79 @@
+//
+//  HomeVC.swift
+//  CoreML-ImageDetector
+//
+//  Created by Maid Mehic on 21/11/2020.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class HomeVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            mainImageView.image = pickedImage
+            
+            guard let ciImage = CIImage(image: pickedImage) else {
+                fatalError("unable to convert UIImage to CIImage")
+            }
+            
+            detect(image: ciImage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("loading CoreML model fail")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (vnRequest, error) in
+            guard let results = vnRequest.results as? [VNClassificationObservation] else {
+                fatalError("result failed to return `VNClassificationObservation` object")
+            }
+            
+            if let firstResult = results.first {
+                self.presentAlert(firstResult.identifier, firstResult.confidence)
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print("failed to perform request")
+        }
+    }
+    
+    func presentAlert(_ title: String, _ confidence: Float) {
+        let confidence = String((confidence * 100.0).rounded())
+        let alert = UIAlertController(title: title, message: "\(confidence)%", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Find out More!", style: .default, handler: { (action) in
+            self.performSegue(withIdentifier: "showMore", sender: self)
+        }))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        DispatchQueue.main.async {
+            self.present(alert, animated: true)
+        }
+    }
+    
+    @IBAction func camereTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
---
+++ CoreML-ImageDetector/Controller/MoreVC.swift
@@ -0,0 +1,16 @@
+//
+//  MoreVC.swift
+//  CoreML-ImageDetector
+//
+//  Created by Maid Mehic on 21/11/2020.
+//
+
+import UIKit
+
+class MoreVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
